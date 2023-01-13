484c1ef11a2ebb7e3452c8ddd8c5b347f7b8fe82
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2023-01-13 15:21:22 +0000

    adding an else if statement if the user finishes in time

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,13 +13,22 @@ function timer() {
     let timerInterval = setInterval(function () {
       seconds --;
       timeRemaining.textContent = "Time left: " + seconds + " s";
-    
+
+    //created an if statement, if time has ran out, to tell the user
       if (seconds <= 0){
         clearInterval(timerInterval);
         timeRemaining.textContent = "Your time has ran out"; 
         finish.textContent = "Your time has ran out";
         finishedQuiz();
 
+    } else  if(questCount >= questionLists.length +1) {
+        clearInterval(timerInterval);
+        finishedQuiz();
+        } 
+}, 1000);
     
     }
+
+function finishedQuiz() {
+
 }
