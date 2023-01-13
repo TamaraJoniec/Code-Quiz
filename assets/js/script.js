8c4d2d7f56b96c7b4712c64418e8942d247e3b92
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2023-01-13 16:42:57 +0000

    // created variables for option buttons, linked back to html file.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 
- // WHEN I click the start button THEN a timer starts 
+// WHEN I click the start button THEN a timer starts 
 
 let startButton = document.querySelector("#start_button");
 let finish = document.querySelector("#finish");
@@ -9,30 +9,31 @@ let question = 0;
 let score = 0;
 let questCount = 1;
 
-function timer() {       
+function timer() {
     let timerInterval = setInterval(function () {
-      seconds --;
-      timeRemaining.textContent = "Time left: " + seconds + " s";
+        seconds--;
+        timeRemaining.textContent = "Time left: " + seconds + " s";
 
-    //created an if statement, if time has ran out, to tell the user
-      if (seconds <= 0){
-        clearInterval(timerInterval);
-        timeRemaining.textContent = "Your time has ran out"; 
-        finish.textContent = "Your time has ran out";
-        finishedQuiz();
-    //adding an else if statement if the user finishes in time
-    } else  if(questCount >= questionLists.length +1) {
-        clearInterval(timerInterval);
-        finishedQuiz();
-        } 
-}, 1000);
+        //created an if statement, if time has ran out, to tell the user
+        if (seconds <= 0) {
+            clearInterval(timerInterval);
+            timeRemaining.textContent = "Your time has ran out";
+            finish.textContent = "Your time has ran out";
+            finishedQuiz();
+            //adding an else if statement if the user finishes in time
+        } else if (questCount >= questionLists.length + 1) {
+            clearInterval(timerInterval);
+            finishedQuiz();
+        }
+    }, 1000);
+}
+//  WHEN a timer starts and I am presented with a question
+let optionButton1 = document.querySelector("#optionButton1");
+let optionButton2 = document.querySelector("#optionButton2");
+let optionButton3 = document.querySelector("#optionButton3");
+let optionButton4 = document.querySelector("#optionButton4");
     
-    }
-  //  WHEN a timer starts and I am presented with a question
-  let choices;
-  let answer;
-
-  let questionLists = [
+let questionLists = [
     {
         question: "Q1 : What's the capital city of Indonesia",
         choices: ["a. Bali", "b. Nusa Penida", "c. Jakarta", "d. Java"],
@@ -54,7 +55,19 @@ function timer() {
         answer: "a"
     },
 ]
-  // finished Quiz function
+// WHEN I answer a question, identify right from wrong answers
+    
+var check = document.querySelector("#check")
+function checkAnswer(event) {
+        event.preventDefault();
+        //show result
+        check.style.display = "block";
+        setTimeout(function () {
+            checkLine.style.display = 'none';
+        }, 1000);
+    }
+
+// finished Quiz function
 function finishedQuiz() {
 
 }
