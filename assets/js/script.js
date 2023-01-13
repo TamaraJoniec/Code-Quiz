63453d7ba2b92b3328818b9857250c24afbfe352
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2023-01-13 14:57:07 +0000

    created variables for starting the timer

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+
+ // WHEN I click the start button THEN a timer starts 
+
+let startButton = document.querySelector("#start_button");
+let finish = document.querySelector("#finish");
+let timeRemaining = document.getElementById("timer");
+let seconds = 60;
+let question = 0;
+let Score = 0;
+let questCount = 1;
+
